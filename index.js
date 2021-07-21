32e20e17f6d2462dda8994889450249a33823e7e
sparshmartires121 <sparsh@applore.in>
2021-07-21 20:11:56 +0530

    proxy

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 var app = express();
-
+var cors = require("cors");
 const port = process.env.PORT||8000;
-
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
