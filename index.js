b1ba5bf1caf0072b17ca2c0de205f6fd0e45ab7c
sparshmartires121 <sparsh@applore.in>
2021-07-21 19:02:22 +0530

    proxy

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+var app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+app.use('/api', createProxyMiddleware({ target: 'https://mailtmc-test.chargebee.com', changeOrigin: true }));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
